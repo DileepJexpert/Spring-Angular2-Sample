53b6c29798f7208fb11d5f5c76ff148e1925daa2
DileepkMaurya <todileepmaurya@gmail.com>
2017-08-31 14:07:07 +0400

    First commit

---
+++ src/main/java/com/jexpert/demo/SpringAngular2SampleApplication.java
@@ -0,0 +1,12 @@
+package com.jexpert.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngular2SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngular2SampleApplication.class, args);
+	}
+}
